0c17d08645fea0ef32a1831f6842790395108dad
ederg <ederyairnglz@gmail.com>
2016-08-13 04:47:23 -0500

    Fix bugs

--- background/background.js
+++ background/background.js
@@ -2,13 +2,11 @@
 	var selector = document.querySelectorAll('td.entry');
 	var icon = ['http://www.meditechsac.com/imagen/check-mark-hi.png'];
 	var episodes = JSON.parse(localStorage.getItem('episode'));
-
 	var saveEpisode = function(name, elm){
 		episodes[name.replace(/\s+/g, '')] = name;
 		elm.src = icon[0];
 		localStorage.setItem('episode', JSON.stringify(episodes));
-	};
-
+	}
 	for(var i = 0; i< selector.length; i++){
 		var episodeId = selector[i].textContent.replace(/\s+/g, '');
 		if (episodes[episodeId] !== undefined) {
@@ -17,10 +15,10 @@
      	}
 		selector[i].addEventListener('click', function(){
 			var that = this;
-			if(that.nextSibling !== null){
+			if(that.nextSibling!== null){
 				var episodeBullet = that.nextSibling.textContent;
 				var bullet = that.children[0];
-				saveEpisode(episodeBullet, elm);
+				saveEpisode(episodeBullet, bullet);
 			}else{
 				var episodeLink = that.textContent;
 				var icon = that.parentNode.children[0].children[0];
